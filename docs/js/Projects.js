1076852250a226f394eb753676b1ecc7a5c3f8f3
Auguste Paccapelo <paccapelo.auguste@gmail.com>
2026-08-28 19:37:11 +0200

    [ADD] Easier paths

--- docs/js/Projects.js
+++ docs/js/Projects.js
@@ -24,23 +24,68 @@ function createCategories(project_categories) {
     }).join(" - ");
 }
 
+function sortProjects(projects, category) {
+    return projects.filter(project => 
+        project.categories.includes(category)
+    );
+}
+
+function getProjectAsset(project, fileName) {
+    return root + project.assets_path + fileName;
+}
+
+function setHeadPage(element, project) {
+    element.innerHTML += `
+        <h1>${project.title}</h1>
+        <h3>${project.date} - ${createCategories(project.categories)} - ${project.duration}</h3>
+        <h1>${project.job}</h1>
+    `;
+}
+
 async function loadProjects() {
     const container = document.getElementById("projects_container");
-    if (container === null) return;
+    const projectId = document.body.dataset.projectId;
+    if (container === null && projectId === null) return;
 
     //Data
     const projects_file = await fetch(root + "Data/Projects.json");
-    const projects = await projects_file.json();
+    const all_projects = await projects_file.json();
 
     //Categories
     const categories_file = await fetch(root + "Data/Categories.json");
     categories = await categories_file.json();
 
-    //Ranks
-    const ranks_file = await fetch(root + "Data/RankedProjects.json");
-    const ranks = await ranks_file.json();
+    let projects = all_projects;
 
-    projects.forEach(project => {
-        container.innerHTML += createProjectPreview(project); 
-    });
+    if (container != null) {
+        const currentCategory = container.dataset.category;
+
+        projects = sortProjects(projects, currentCategory);
+
+        //Ranks
+        const ranks_file = await fetch(root + "Data/RankedProjects.json");
+        const ranks = await ranks_file.json();
+
+        projects.forEach(project => {
+            container.innerHTML += createProjectPreview(project); 
+        });
+    }
+
+    if (projectId) {
+        const project = projects.find(project => project.id === projectId);
+
+        const headPage = document.querySelector(".head-page");
+        setHeadPage(headPage, project);
+
+        document.querySelectorAll("[data-src]").forEach(element => {
+            const fileName = element.dataset.src;
+            const assetPath = getProjectAsset(project, fileName);
+            element.src = assetPath;            
+            
+            const video = element.closest("video");
+            if (video) {
+                video.load();
+            }
+        });
+    }
 }
